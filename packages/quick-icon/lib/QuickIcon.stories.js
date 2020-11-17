c0a9152788b95836363f07b2c8ab38e5676b9204
Thomas Rokicki <tcrokicki@gmail.com>
2020-11-17 03:38:28 -0500

    Attempt to understand storyboard with QuickIcon

--- packages/quick-icon/lib/QuickIcon.stories.js
+++ packages/quick-icon/lib/QuickIcon.stories.js
@@ -1,11 +1,11 @@
-// import { storiesOf } from '@storybook/vue'
-// import QuickIcon from './QuickIcon'
+import { storiesOf } from '@storybook/vue'
+import QuickIcon from './QuickIcon.vue'
 
-// storiesOf('QuickIcon', module)
-//   .add('normal', () => ({
-//     components: { QuickIcon },
-//     template:
-//     `
-//     <Icon :name="'example-image'" :width="'20'" :size="1" viewBox="0 9 32 32" />
-//     `
-//   }))
+storiesOf('QuickIcon', module)
+  .add('normal', () => ({
+    components: { QuickIcon },
+    template:
+    `
+    <QuickIcon :name="'check-icon'" :width="'20'" :size="'1'" viewBox="0 9 32 32" />
+    `
+  }))
